83e499f0da10f47f3c02ae46b54b709b1f439d1d
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-11-27 18:24:55 +0100

    Adding classes

---
+++ src/main/groovy/de/dfki/mary/htspacking/HTSPackingPlugin.groovy
@@ -0,0 +1,130 @@
+package de.dfki.mary.htspacking
+
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.MavenPlugin
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.bundling.Zip
+
+import static groovyx.gpars.GParsPool.runForkJoin
+import static groovyx.gpars.GParsPool.withPool
+
+import de.dfki.mary.htspacking.*
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import groovy.xml.*
+
+class HTSPackingPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.plugins.apply JavaPlugin
+        project.plugins.apply MavenPlugin
+
+        project.sourceCompatibility = JavaVersion.VERSION_1_7
+
+        // Load configuration
+        def slurper = new JsonSlurper()
+        def config_file = project.rootProject.ext.config_file
+        def config = slurper.parseText( config_file.text )
+
+        // Adapt pathes
+        DataFileFinder.project_path = new File(getClass().protectionDomain.codeSource.location.path).parent
+        if (config.data.project_dir) {
+            DataFileFinder.project_path = config.data.project_dir
+        }
+
+        def beams = config.settings.training.beam.split() as List
+        def nb_proc_local = 1
+        if (project.gradle.startParameter.getMaxWorkerCount() != 0) {
+            nb_proc_local = Runtime.getRuntime().availableProcessors(); // By default the number of core
+            if (config.settings.nb_proc) {
+                if (config.settings.nb_proc > nb_proc_local) {
+                    throw Exception("You will overload your machine, preventing stop !")
+                }
+
+                nb_proc_local = config.settings.nb_proc
+            }
+        }
+
+        project.ext {
+            maryttsVersion = '5.1.2'
+
+            // User configuration
+            user_configuration = config;
+
+            trained_files = new HashMap()
+
+
+            // Nb processes
+            nb_proc = nb_proc_local
+
+            // HTS wrapper
+            utils_dir = "$project.buildDir/tmp/utils"
+
+            template_dir = "$project.buildDir/tmp/templates"
+
+
+            input_file = DataFileFinder.getFilePath(config.data.wav_dir) + "/${project.name}.wav"
+            basename = project.name
+        }
+
+        project.status = project.version.endsWith('SNAPSHOT') ? 'integration' : 'release'
+
+        project.repositories {
+            jcenter()
+            maven {
+                url 'http://oss.jfrog.org/artifactory/repo'
+            }
+        }
+
+        project.configurations.create 'legacy'
+
+        project.sourceSets {
+            main {
+                java {
+                    // srcDir project.generatedSrcDir
+                }
+            }
+            test {
+                java {
+                    // srcDir project.generatedTestSrcDir
+                }
+            }
+        }
+
+        project.afterEvaluate {
+
+            /**
+             * CMP generation task
+             */
+            project.task('generateCMP') {
+                (new File("$project.buildDir/cmp")).mkdirs()
+                outputs.files "$project.buildDir/cmp" + project.basename + ".cmp"
+
+                def extToDir = new Hashtable<String, String>()
+                extToDir.put("cmp".toString(), "$project.buildDir/cmp".toString())
+
+                config.models.cmp.streams.each  { stream ->
+                    def kind = stream.kind
+                    extToDir.put(kind.toLowerCase().toString(),
+                                 (("$project.buildDir/" + kind.toLowerCase()).toString()))
+                }
+
+                doLast {
+
+                    def extractor = new ExtractCMP(config_file.toString())
+                    extractor.setDirectories(extToDir)
+                    extractor.extract("$project.basename")
+                }
+            }
+        }
+    }
+
+}
