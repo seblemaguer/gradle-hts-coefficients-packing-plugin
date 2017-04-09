2b786781695c35f624c2d4390de42b23664db457
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-04-09 15:39:19 +0200

    starting to support hook

--- src/main/groovy/de/dfki/mary/htspacking/HTSPackingPlugin.groovy
+++ src/main/groovy/de/dfki/mary/htspacking/HTSPackingPlugin.groovy
@@ -29,83 +29,34 @@ class HTSPackingPlugin implements Plugin<Project> {
 
         project.sourceCompatibility = JavaVersion.VERSION_1_7
 
-        // Load configuration
-        def slurper = new JsonSlurper()
-        def config_file = project.rootProject.ext.config_file
-        def config = slurper.parseText( config_file.text )
-
-        // Adapt pathes
-        DataFileFinder.project_path = new File(getClass().protectionDomain.codeSource.location.path).parent
-        if (config.data.project_dir) {
-            DataFileFinder.project_path = config.data.project_dir
-        }
-
-        def nb_proc_local = 1
-        if (project.gradle.startParameter.getMaxWorkerCount() != 0) {
-            nb_proc_local = Runtime.getRuntime().availableProcessors(); // By default the number of core
-            if (config.settings.nb_proc) {
-                if (config.settings.nb_proc > nb_proc_local) {
-                    throw Exception("You will overload your machine, preventing stop !")
-                }
-
-                nb_proc_local = config.settings.nb_proc
-            }
-        }
-
         project.ext {
-            maryttsVersion = '5.1.2'
-
-            // User configuration
-            user_configuration = config;
-
-            trained_files = new HashMap()
-
-
-            // Nb processes
-            nb_proc = nb_proc_local
+            basename = project.name
 
             // HTS wrapper
             utils_dir = "$project.buildDir/tmp/utils"
-
             template_dir = "$project.buildDir/tmp/templates"
-
-            basename = project.name
-        }
-
-        project.status = project.version.endsWith('SNAPSHOT') ? 'integration' : 'release'
-
-        project.repositories {
-            jcenter()
-            maven {
-                url 'http://oss.jfrog.org/artifactory/repo'
-            }
-        }
-
-        project.configurations.create 'legacy'
-
-        project.sourceSets {
-            main {
-                java {
-                    // srcDir project.generatedSrcDir
-                }
-            }
-            test {
-                java {
-                    // srcDir project.generatedTestSrcDir
-                }
-            }
-        }
-
-        project.task('prepareEnvironment') {
         }
 
         project.afterEvaluate {
+
+            project.task('configuration') {
+
+                // User configuration
+                ext.user_configuration = project.configurationPacking.hasProperty("user_configuration") ? project.configurationPacking.user_configuration : null
+                ext.config_file = project.configurationPacking.hasProperty("config_file") ? project.configurationPacking.config_file : null
+                ext.trained_files = new HashMap()
+
+                // Nb processes
+                ext.nb_proc = project.configurationPacking.hasProperty("nb_proc") ? project.configurationPacking.nb_proc : 1
+
+            }
+
 
             /**
              * CMP generation task
              */
             project.task('generateCMP') {
-                dependsOn "prepareEnvironment"
+                dependsOn "configuration"
                 outputs.files "$project.buildDir/cmp" + project.basename + ".cmp"
 
 
@@ -115,12 +66,12 @@ class HTSPackingPlugin implements Plugin<Project> {
                     def extToDir = new Hashtable<String, String>()
                     extToDir.put("cmp".toString(), "$project.buildDir/cmp".toString())
 
-                    project.user_configuration.models.cmp.streams.each  { stream ->
+                    project.configuration.user_configuration.models.cmp.streams.each  { stream ->
                         def kind = stream.kind
                         extToDir.put(kind.toLowerCase().toString(), stream.coeffDir.toString())
                     }
 
-                    def extractor = new ExtractCMP(config_file.toString())
+                    def extractor = new ExtractCMP(project.configuration.config_file.toString())
                     extractor.setDirectories(extToDir)
                     extractor.extract("$project.basename")
                 }
@@ -130,7 +81,7 @@ class HTSPackingPlugin implements Plugin<Project> {
              * FFO generation task
              */
             project.task('generateFFO') {
-                dependsOn "prepareEnvironment"
+                dependsOn "configuration"
                 outputs.files "$project.buildDir/ffo" + project.basename + ".ffo"
 
 
@@ -141,22 +92,22 @@ class HTSPackingPlugin implements Plugin<Project> {
                     def extToDir = new Hashtable<String, String>()
                     extToDir.put("ffo".toString(), "$project.buildDir/ffo".toString())
 
-                    project.user_configuration.models.ffo.streams.each  { stream ->
+                    project.configuration.user_configuration.models.ffo.streams.each  { stream ->
                         def kind = stream.kind
                         extToDir.put(kind.toLowerCase().toString(), stream.coeffDir.toString())
                     }
 
-                    def extractor = new ExtractFFO(config_file.toString())
+                    def extractor = new ExtractFFO(project.configuration.config_file.toString())
                     extractor.setDirectories(extToDir)
                     extractor.extract("$project.basename")
                 }
             }
 
             project.task('pack') {
-                if (project.user_configuration.models.cmp) {
+                if (project.configuration.user_configuration.models.cmp) {
                     dependsOn "generateCMP"
                 }
-                if (project.user_configuration.models.ffo) {
+                if (project.configuration.user_configuration.models.ffo) {
                     dependsOn "generateFFO"
                 }
             }
